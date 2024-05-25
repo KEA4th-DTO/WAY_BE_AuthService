80f9c4b2f48fb33aec5bb645f34a82e1c92cf17f
Haecheol Shin <dev.hc.shin@gmail.com>
2024-05-26 02:46:48 +0900

    Fix: 환경 변수 순환 참조 오류 수정

--- src/main/java/com/dto/way/auth/domain/service/MailService.java
+++ src/main/java/com/dto/way/auth/domain/service/MailService.java
@@ -23,7 +23,7 @@ public class MailService {
     private final CertificationGenerator certificationGenerator;
     private final RedisService redisService;
 
-    @Value("${MAIL-LOCATION}")
+    @Value("${VERIFY-LOCATION}")
     private String location;
 
     public EmailCertificationResponse sendEmailForCertification(String email) throws NoSuchAlgorithmException, MessagingException {

97ea72859cadc6303c76ea5ac7ab0b1adc12908e
Haecheol Shin <dev.hc.shin@gmail.com>
2024-05-26 02:25:19 +0900

    Fix: 메일 서버 관련 오류 수정

--- src/main/java/com/dto/way/auth/domain/service/MailService.java
+++ src/main/java/com/dto/way/auth/domain/service/MailService.java
@@ -23,7 +23,7 @@ public class MailService {
     private final CertificationGenerator certificationGenerator;
     private final RedisService redisService;
 
-    @Value("${MAIL_LOCATION}")
+    @Value("${MAIL-LOCATION}")
     private String location;
 
     public EmailCertificationResponse sendEmailForCertification(String email) throws NoSuchAlgorithmException, MessagingException {

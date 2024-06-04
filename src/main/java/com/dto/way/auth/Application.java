a251df1f348f9e9792b5b9e1fbed9095f005e1a2
Haecheol Shin <dev.hc.shin@gmail.com>
2024-06-04 17:33:54 +0900

    Fix: 서울 시간대로 Timezone 변경

--- src/main/java/com/dto/way/auth/Application.java
+++ src/main/java/com/dto/way/auth/Application.java
@@ -1,7 +1,10 @@
 package com.dto.way.auth;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,12 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// ------이 부분을 추가한다----------
+	@PostConstruct
+	public void init() {
+		// timezone 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("Current TimeZone: " + TimeZone.getDefault().getID());
+	}
+	//-------------------------------
 }
